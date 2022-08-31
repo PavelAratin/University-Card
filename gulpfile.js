5751d75053ff69eff7c258171c4eb0cd8f3b0e32
Pavel <paha_gagarin@mail.ru>
2022-08-31 14:55:33 +0300

    верстка карточки учебного заведения

---
+++ gulpfile.js
@@ -0,0 +1,199 @@
+//экспорт дефолтных методов галпа
+const { src, dest, parallel, series, watch } = require('gulp');
+//установленные модули
+const sass = require('gulp-sass')(require('sass'));//препроцессор
+const notify = require('gulp-notify');//оповещение об ошибках в scss
+const autoprefixer = require('gulp-autoprefixer');//автопрефиксер
+const sourcemaps = require('gulp-sourcemaps');// отслеживание ошибок в разных файлах
+const browserSync = require('browser-sync').create(); //браузерсинк - автоперезагрузка браузера
+const fileInclude = require('gulp-file-include'); // позволяет вкладывать файлы друг в друга
+const svgSprite = require('gulp-svg-sprite');// работа с свг-спрайтами
+const ttf2woff = require('gulp-ttf2woff'); //конвертация шрифта
+const ttf2woff2 = require('gulp-ttf2woff2'); //конвертация шрифта
+const del = require('del');//удаление папки app после запуска галп
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');// слежение за js фалами
+const tinyPng = require('gulp-tinypng-compress'); //сжатие фотографий в build-версии
+
+//функция для конвертации шрифтов и перегонки шрифтов
+const fonts = () => {
+  src('./src/fonts/**.ttf')
+    .pipe(ttf2woff())
+    .pipe(dest('./app/fonts/'))
+  return src('./src/fonts/**.ttf')
+    .pipe(ttf2woff2())
+    .pipe(dest('./app/fonts/'))
+}
+//функция для работы с свг-спрайтами
+const svgSprites = () => {
+  return src('./src/img/**.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: '../sprite.svg'
+        }
+      }
+    }))
+    .pipe(dest('./app/img'))
+}
+//функция для scss
+const styles = () => {
+  return src('./src/scss/**.scss')
+    .pipe(sourcemaps.init())//начало отслеживание ошибок в разхных вайлах scss
+    .pipe(sass().on('error', notify.onError()))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(sourcemaps.write('.')) //отслеживание ошибок в разхных вайлах scss
+    .pipe(dest('./app/css'))
+    .pipe(browserSync.stream()) //отслеживание файлов в реальном времени
+}
+
+//функция для работы с html
+const htmlInclude = () => {
+  return src(['./src/index.html'])
+    .pipe(fileInclude({
+      prefix: '@',
+      basepath: '@file'
+    }))
+    .pipe(dest('./app'))
+    .pipe(browserSync.stream()); //отслеживание файлов в реальном времени
+}
+//простая перегонка картинок для dev-версии (в процессе разработки)
+const imgToApp = () => {
+  return src(['./src/img/**.jpg', './src/img/**.png', './src/img/**.jpeg', './src/img/**.svg'])
+    .pipe(dest('./app/img'))
+}
+//функция для удаления папки app
+const clean = () => {
+  return del(['./app/*'])
+}
+
+//перегонка библиотеки chart.js
+const chartToApp = () => {
+  return src(['./src/chart/*'])
+    .pipe(dest('./app/chart'))
+}
+
+const faviconToApp = () => {
+  return src(['./src/favicon.ico'])
+    .pipe(dest('./app/'))
+}
+
+const swiperToApp = () => {
+  return src(['./src/swiper/*'])
+    .pipe(dest('./app/swiper'))
+}
+
+//функция для работы со скриптами
+const scipts = () => {
+  return src('./src/js/main.js')
+    .pipe(webpackStream({
+      mode: 'development',//режим разработки
+      output: {
+        filename: 'main.js',
+      },
+      devtool: 'source-map',//карта проетк(из каких кусочков состоит)
+      module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', {
+                    debug: true,//вывод ошибок в консоль
+                  }]
+                ]
+              }
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('./app/js'))
+    .pipe(browserSync.stream());
+}
+
+//функция для слежения за файлами 
+const watchFiles = () => {
+  browserSync.init({
+    server: {
+      baseDir: './app' //базовый сервер для слежения установлен за всей папкой app
+    }
+  });
+  //watch - дефолтный метод галпа
+  watch('./src/scss/**/*.scss', styles);//как только будут изменения в стилях, вызовется функция styles
+  watch('./src/index.html', htmlInclude);
+  watch('./src/img**.jpg', imgToApp);
+  watch('./src/img**.png', imgToApp);
+  watch('./src/img**.jpeg', imgToApp);
+  watch('./src/img**.svg', svgSprites);
+  watch('./src/fonts/**.ttf', fonts);
+  watch('./src/js/**/*.js', scipts);
+}
+
+//делаем функции тасками
+exports.styles = styles;
+exports.fileInclude = htmlInclude;
+exports.fonts = fonts;
+exports.clean = clean;
+exports.watchFiles = watchFiles;
+exports.chartToApp = chartToApp;
+exports.faviconToApp = faviconToApp;
+exports.swiperToApp = swiperToApp;
+
+//в дефолтном таске мы используем функции(вызываются первый раз перед вотчингом)
+exports.default = series(clean, parallel(htmlInclude, scipts, chartToApp,swiperToApp, fonts, imgToApp, faviconToApp, svgSprites), styles, watchFiles);
+//код для build-версии
+//функция для работы со скриптами
+const sciptsBuild = () => {
+  return src('./src/js/main.js')
+    .pipe(webpackStream({
+      output: {
+        filename: 'main.js',
+      },
+      module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(dest('./app/js'))
+}
+
+//функция для scss
+const stylesBuild = () => {
+  return src('./src/scss/**.scss')
+    .pipe(sass().on('error', notify.onError()))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(dest('./app/css'))
+}
+//сжатие фотографий
+const tinypng = () => {
+  return src(['./src/img/**.jpg', './src/img/**.png', './src/img/**.jpeg'])
+    .pipe(tinyPng({
+      key: 'JS9MWwCflHycV1FDcSlkjZ8GGjTfttNw',
+      log: true
+    }))
+    .pipe(dest('./app/img'))
+}
+//dev-сборка
+exports.build = series(clean, parallel(htmlInclude, sciptsBuild, chartToApp,swiperToApp, fonts, imgToApp, faviconToApp, svgSprites), stylesBuild, tinypng);
---
+++ src/js/main.js
@@ -0,0 +1,253 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById('grafic').getContext('2d');
+  const slider = document.querySelector('.js-swiper');
+  const contentBlock = document.querySelector('.js-content');
+  const bodyBlock = document.querySelector('.js-body');
+  const contentLeftBlock = document.querySelector('.js-content-left');
+  const allcityBlock = document.querySelector('.js-allcity-block');
+  const locationsDropdownBlock = document.querySelector('.js-locations-dropdown');
+  const locationTown = document.querySelector('.js-location-town');
+  const fetchUrl = 'https://api.hh.ru/areas/113';
+  let mySwiper;
+  ctx.canvas.parentNode.style.height = "307px";
+
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
+      datasets: [
+        {
+          label: 'Проходной бал на бюджет',
+          backgroundColor: 'rgba(6, 86, 180, 0.6)',
+          // borderColor: 'rgba(6, 86, 180, 0.6)',
+          data: [30, 110, 130, 210, 50, 160, 140, 10, 11, 155],
+          fill: true,
+          pointBackgroundColor: '#ffffff',
+          pointBorderColor: 'rgba(6, 86, 180, 0.6)',
+          pointHoverBackgroundColor: 'rgba(6, 86, 180, 0.6)',
+          pointHoverBorderWidth: 4
+        },
+        {
+          label: 'Проходной бал на бюджет',
+          backgroundColor: 'rgba(171, 200, 234, 0.6)',
+          borderColor: 'rgb(36,162,254)',
+          borderWidth: 1,
+          data: [110, 200, 102, 215, 160, 140, 205, 160, 0, 200],
+          fill: true,
+          pointBackgroundColor: '#ffffff',
+          pointBorderColor: 'rgba(6, 86, 180, 0.6)',
+          pointHoverBackgroundColor: 'rgba(6, 86, 180, 0.6)',
+          pointHoverBorderWidth: 4
+        }
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          yAlign: 'bottom',
+          displayColors: false,
+          padding: 26,
+          backgroundColor: 'white',
+          bodyColor: '#000000',
+          callbacks: {
+            title: function (context) {
+              return ''
+            },
+          }
+        }
+      }
+    }
+  })
+
+  function mobileSlider() {
+    if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
+      mySwiper = new Swiper(slider, {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        wrapperClass: 'speciality-swiper__wrapper',
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true
+        },
+      });
+      slider.dataset.mobile == 'true'
+      slider.querySelector('.speciality-swiper__wrapper').classList.remove('active')
+    }
+    if (window.innerWidth > 768) {
+      slider.dataset.mobile == 'false';
+      if (slider.classList.contains('swiper-initialized')) {
+        slider.querySelector('.speciality-swiper__wrapper').classList.add('active')
+        mySwiper.destroy(false, true)
+      }
+    }
+  }
+  mobileSlider()
+
+  function fixMenuBlock() {
+    let scrollDistance = window.scrollY;
+    let scrollMenuBlock = document.querySelector('.js-scroll-menu')
+    let scrollMenuHtml = `
+    <div class="scroll-menu js-scroll-menu">
+    <div class="container scroll-menu__container">
+      <div class="scroll-menu__wrapper">
+        <p class="scroll-menu__title">Московский государственный технический университет имени Н.Э.Бáумана</p>
+        <ul class="adress-list adress-list__scroll-menu">
+          <li class="adress-list__item">
+            <p class="adress-list__text">Москва, ул. Новочеремушкинская, д. 69</p>
+          </li>
+          <li class="adress-list__item">
+            <p class="adress-list__text">Приемная комиссия: <a href="tel:+74991812133">7 (499) 181 21 33</a></p>
+          </li>
+        </ul>
+      </div>
+      <button class="button-in-favorites scroll-menu__button-in-favorites">
+        <svg class="heart-svg hero__heart-svg">
+          <use xlink:href="img/sprite.svg#heart"></use>
+        </svg>
+        <span>В Избранное</span>
+      </button>
+    </div>
+  </div>
+    `;
+    if (scrollDistance >= contentBlock.offsetTop - 97) {
+      bodyBlock.insertAdjacentHTML('afterbegin', scrollMenuHtml);
+      contentLeftBlock.classList.add('fix')
+    } else {
+      contentLeftBlock.classList.remove('fix')
+
+    }
+    if (scrollMenuBlock) {
+      scrollMenuBlock.remove()
+    }
+  }
+
+  window.addEventListener('resize', () => {
+    mobileSlider();
+  });
+
+  window.addEventListener('scroll', () => {
+    fixMenuBlock()
+  });
+
+  function htmlCityItem(data) {
+    return `
+    <div class="locations-dropdown__citys">
+      <span class="locations-dropdown__city-name">${data.areas[0].name ? data.areas[0].name : ''}</span>
+      <span class="locations-dropdown__city-state">${data.name}</span>
+    </div>
+    `
+  }
+
+  locationTown.addEventListener('click', function () {
+    let input = document.querySelector('.locations-dropdown__input').focus()
+    locationsDropdownBlock.classList.toggle('active')
+    fetch(fetchUrl)
+      .then((response) => {
+        return response.json()
+      })
+      .then((body) => {
+        let areas = body.areas
+        setTimeout(function () {
+          document.querySelector('.spinner').remove()
+          areas.forEach(function (item) {
+            allcityBlock.innerHTML += htmlCityItem(item)
+            let cityNames = document.querySelectorAll('.locations-dropdown__city-name')
+            searctCity(cityNames)
+          })
+        }, 2000)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  })
+
+  document.addEventListener('click', function (e) {
+    if (!e.target.closest('.header__locations')) {
+      locationsDropdownBlock.classList.remove('active')
+    }
+  })
+
+
+  function blockSpinner() {
+    const html = `
+     <div class="spinner">
+        <img src="img/spinner.svg"></img>
+     </div>
+    `
+    allcityBlock.insertAdjacentHTML('beforebegin', html)
+  }
+  blockSpinner()
+
+  function searctCity(citys) {
+    let input = document.querySelector('.locations-dropdown__input')
+    let dropdownCitys = document.querySelectorAll('.locations-dropdown__citys')
+    input.oninput = function () {
+      let valueInput = this.value.trim();
+      if (valueInput != '') {
+        citys.forEach(function (city) {
+          if (city.innerText.search(valueInput) == -1) {
+            city.closest('.locations-dropdown__citys').classList.add('hide')
+          }
+        });
+      } else {
+        citys.forEach(function (city) {
+          city.closest('.locations-dropdown__citys').classList.remove('hide')
+        })
+      }
+      input.addEventListener('keyup', function (e) {
+        if (e.keyCode === 8) {
+          citys.forEach(function (city) {
+            if (city.innerText.search(valueInput) == -1) {
+              city.closest('.locations-dropdown__citys').classList.remove('hide')
+            }
+          });
+        }
+      })
+    }
+    const selectedSity = document.querySelector('.locations-dropdown__selected-city')
+
+    dropdownCitys.forEach(function (item) {
+      item.addEventListener('click', function () {
+        let button = document.createElement('button')
+        button.classList.add('locations-dropdown__button')
+        button.classList.add('locations-dropdown__button--shields')
+        button.innerHTML = item.querySelector('.locations-dropdown__city-name').textContent
+        selectedSity.appendChild(button)
+      })
+    })
+  }
+  const navListWrapper = document.querySelector('.js-nav-list');
+  const navItems = document.querySelectorAll('.nav__item');
+  const navButtons = document.querySelectorAll('.nav__button');
+  let count = 10;
+
+  navButtons.forEach(function (btn, index) {
+    if (index === 1) {
+      btn.classList.remove('disactive')
+    }
+    btn.addEventListener('click', function () {
+      let sumWidth = 0
+      navItems.forEach(function (item, index) {
+        if (index + 1 >= count) {
+          sumWidth += Math.floor(item.getBoundingClientRect().width)
+        }
+      });
+      navListWrapper.style.cssText = `transform:translateX(${-sumWidth - (-90)}px);`;
+      navButtons.forEach(function (button) {
+        button.classList.remove('disactive')
+      });
+      this.classList.add('disactive');
+      if(btn.classList.contains('nav__button--left')){
+        console.log(sumWidth / 2)
+        navListWrapper.style.cssText = `transform:translateX(${0}px);`;
+      }
+
+    });
+  })
+})
